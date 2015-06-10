04d52add33caa000d82e62dd65aacd885fd46f4c
Luiz Guilherme <santos.uff@gmail.com>
2015-06-10 20:07:16 -0300

    Think is tweaked now. Added support for long and small phonemes also.

--- imageprototype.py
+++ imageprototype.py
@@ -6,10 +6,13 @@ import sys
 import nltk
 import phonemes
 import thread
+import time
 from  AppKit import NSSpeechSynthesizer
 
 #Defines
-timePerPhoneme = 0.2
+timePerPhoneme = 0.15
+longPhonemeBonus = 0.05
+smallPhonemeBonus = -0.05
 
 # called by each thread
 def speak_word(word, waittime):
@@ -29,7 +32,7 @@ print tokens
 entries = nltk.corpus.cmudict.entries()
 print len(entries)
 
-
+start_time = time.time()
 TextToSpeech = []
 for token in tokens:
     for entry in entries:
@@ -37,37 +40,51 @@ for token in tokens:
             #print entry
             TextToSpeech.append(entry)
             break
+end_time = time.time()
 #Debug!
-print TextToSpeech
-print phonemes.mouths
-print phonemes.PhonemeToMouth
+print "Pre-processing Done! Time: %s Seconds"%(end_time - start_time)
+#print TextToSpeech
+#print phonemes.mouths
+#print phonemes.PhonemeToMouth
 
 
 ## For each mouth you should put a refering mouth to show
 ## do the double check
 for word in TextToSpeech:
-    cv2.imshow('window', phonemes.mouths['blair_rest.jpg'])
-    cv2.waitKey(1)
+    # cv2.imshow('window', phonemes.mouths['blair_rest.jpg'])
+    # cv2.waitKey(1)
     # Get the right list of mouths
 
+    timeForAWord = 0.0
+    timeOfThisPhoneme = 0.0
     MouthsToShow = []
     for ph in word[1]:
         for key, value in phonemes.PhonemeToMouth.items():
+            if '1' in ph:
+                timeForAWord += timePerPhoneme + longPhonemeBonus
+                timeOfThisPhoneme = timePerPhoneme + longPhonemeBonus
+            elif '0' in ph:
+                timeForAWord += timePerPhoneme + smallPhonemeBonus
+                timeOfThisPhoneme = timePerPhoneme + smallPhonemeBonus
+            else:
+                timeForAWord += timePerPhoneme
+                timeOfThisPhoneme = timePerPhoneme
             if key in ph:
-                MouthsToShow.append(value)
+                MouthsToShow.append((value, timeOfThisPhoneme))
+
 
     #Debug!
-    print MouthsToShow
+    #print MouthsToShow
 
     # Call co-routine to speak
     try:
-        thread.start_new_thread(speak_word, (word[0], len(word[1]) * timePerPhoneme ) )
+        t = thread.start_new_thread(speak_word, (word[0], timeForAWord ) )
     except:
         print "Error: unable to start thread for audio =/"
 
     #Show Mouths
     for mouth in MouthsToShow:
-        img = phonemes.mouths[mouth]
+        img = phonemes.mouths[mouth[0]]
         cv2.imshow('window', img)
         cv2.waitKey(1)
-        time.sleep(timePerPhoneme)
+        time.sleep(mouth[1])
